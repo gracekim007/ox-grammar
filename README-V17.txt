4a6c585be103ac73694be3b0c846c2a12e60dacd
gracekim007 <gracekim007s@gmail.com>
2026-03-12 14:40:26 +0900

    Add files via upload

---
+++ README-V17.txt
@@ -0,0 +1,1 @@
+v17 fix: declare/init STUDY and cache-bust app-v17.js
--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 /* OX 문법 PWA service worker */
 
-const CACHE_NAME = 'ox-grammar-cache-v14-rootfix';
+const CACHE_NAME = 'ox-grammar-cache-v17-studyfix';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './app.js',
+  './app-v17.js',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png'
